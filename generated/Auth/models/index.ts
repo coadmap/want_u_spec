dad52ef17cddd04652861c4e034a8c865655dc9a
nozomi-iida <iida19990106@gmail.com>
2022-02-07 14:41:32 +0900

    feat: open APIの作り直し

---
+++ generated/Auth/models/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Account';
+export * from './InlineObject';
+export * from './InlineObject1';
+export * from './SignInAccount';
+export * from './SignInResponse';
+export * from './SignUpResponse';
